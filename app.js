c7571dfacdc5567e2b770d5184c2444b04936ac4
kyiafree <110514670+kyiafree@users.noreply.github.com>
2022-08-28 19:37:46 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const changeColor = () => {
+    let screen = document.querySelector('.name')
+    screen.classList.toggle('test')
+}
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
